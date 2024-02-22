6429d5007c5f8458bf57fa9ae80491cdb57a709c
z69yuan <z69yuan@163.com>
2024-02-22 23:53:07 +0800

    project init

---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+//        maven {
+//            uri('https://maven.aliyun.com/nexus/content/public')
+//        }
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+//        maven {
+//            uri('https://maven.aliyun.com/nexus/content/public')
+//        }
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "LearnAndroidArch"
+include(":app")
+ 
